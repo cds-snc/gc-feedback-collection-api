30cab08a71ed6364a0a23eb8b8f1ed850d6cb9b1
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2025-11-10 13:13:06 -0500

    feat: add SES email receipt configuration

---
+++ terragrunt/aws/ses/inputs.tf
@@ -0,0 +1,15 @@
+ variable "ses_domain" {
+  description = "Domain for SES email receipt (e.g., feedback.canada.gc.ca)"
+  type        = string
+  default     = "feedback.canada.gc.ca"
+}
+
+variable "problem_sns_topic_arn" {
+  description = "ARN of the problem SNS topic"
+  type        = string
+}
+
+variable "toptask_sns_topic_arn" {
+  description = "ARN of the toptask SNS topic"
+  type        = string
+}
