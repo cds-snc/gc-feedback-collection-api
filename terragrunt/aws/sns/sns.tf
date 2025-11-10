75aef79535caf813da2361bfb7a0ce293f7aa9bb
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2025-11-10 13:10:18 -0500

    feat: add SNS topics using CDS modules

---
+++ terragrunt/aws/sns/sns.tf
@@ -0,0 +1,25 @@
+# SNS Topics for email routing to Lambda functions using CDS terraform-modules
+
+# Problem feedback SNS topic (receives from SES)
+module "sns_problem_topic" {
+  source = "github.com/cds-snc/terraform-modules//sns?ref=v10.8.5"
+
+  name              = "${var.product_name}-problem-topic"
+  billing_tag_key   = "CostCentre"
+  billing_tag_value = var.billing_code
+  kms_event_sources = ["ses.amazonaws.com"]
+}
+
+# TopTask survey SNS topic (receives from SES)
+module "sns_toptask_topic" {
+  source = "github.com/cds-snc/terraform-modules//sns?ref=v10.8.5"
+
+  name              = "${var.product_name}-toptask-topic"
+  billing_tag_key   = "CostCentre"
+  billing_tag_value = var.billing_code
+  kms_event_sources = ["ses.amazonaws.com"]
+}
+
+# Note: SNS to SQS subscriptions are created by the SQS module (sqs.tf)
+# Note: Lambda subscriptions are handled automatically by the CDS Lambda module
+# via the sns_topic_arns parameter in lambda.tf
