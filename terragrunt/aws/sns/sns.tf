c1bfe2da36bbbe6a0e0b83e4bd0e7c9d2d9382a2
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2025-11-13 14:49:51 -0500

    fix: use policy parameter in SNS module for SES permissions

--- terragrunt/aws/sns/sns.tf
+++ terragrunt/aws/sns/sns.tf
@@ -1,3 +1,34 @@
+# Data source to construct the SNS topic policy for SES access
+data "aws_iam_policy_document" "sns_topic_policy" {
+  statement {
+    sid    = "AllowAccountOwner"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.account_id}:root"]
+    }
+    actions   = ["SNS:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "AllowSESPublish"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["ses.amazonaws.com"]
+    }
+    actions   = ["SNS:Publish"]
+    resources = ["*"]
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceAccount"
+      values   = [var.account_id]
+    }
+  }
+}
+
+# Problem email SNS topic (receives from SES)
 module "sns_problem_topic" {
   source = "github.com/cds-snc/terraform-modules//sns?ref=v10.8.5"
 
@@ -5,6 +36,7 @@ module "sns_problem_topic" {
   billing_tag_key   = "CostCentre"
   billing_tag_value = var.billing_code
   kms_event_sources = ["ses.amazonaws.com"]
+  policy            = data.aws_iam_policy_document.sns_topic_policy.json
 }
 
 # TopTask survey SNS topic (receives from SES)
@@ -15,6 +47,7 @@ module "sns_toptask_topic" {
   billing_tag_key   = "CostCentre"
   billing_tag_value = var.billing_code
   kms_event_sources = ["ses.amazonaws.com"]
+  policy            = data.aws_iam_policy_document.sns_topic_policy.json
 }
 
 # SNS topic policy to allow SES to publish
